5d11fb7c2d53e6e3fe8e1a88213a5b8539d6e05c
Seungwon Park <psw14041@gmail.com>
2017-02-08 23:49:21 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,34 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="UTF-8">
-  <title>SNU Physics Board RSS</title>
+    <meta charset="UTF-8">
+    <title>SNU Physics Board RSS</title>
 </head>
 
 <body>
 
-<ul>
-  <li> <a href="/notices.xml">Notices</a> </li>
-  <li> <a href="/general.xml">General</a> </li>
-</ul>
+<div align="center">
 
-Warning : This service can be discontinued any time.
+<h2> 서울대 물리학과 게시판 RSS </h2>
+<h2> SNU Physics Board RSS </h2>
 
-경고 : 이 서비스는 언제든지 중단될 수 있습니다.
+<h3>
+<a href="notices.xml" target="_blank">공지(Notices)</a> <br>
+<a href="general.xml" target="_blank">일반(General)</a> <br>
+</h3>
 
+경고 : 이 서비스는 언제든지 중단될 수 있습니다. <br>
+Warning : This service can be discontinued any time. <br>
+
+<br>
+<small>
+만든이 : 박승원(서울대학교 물리천문학부 물리학전공 17학번) / yyyyy@snu.ac.kr <br>
+Credit : Seungwon Park(SNU Dept. of Physics/Astronomy Physics major, '17) / yyyyy@snu.ac.kr <br>
+</small>
+<br>
+<a href="https://github.com/seungwonpark/SNU_physics_board_rss">This project on GitHub</a>
+
+</div>
 </body>
+
 </html>
