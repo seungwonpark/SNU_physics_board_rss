1236daccfe9c719ee859eaf61c5a4649c53ad57f
Seungwon Park <psw14041@gmail.com>
2017-02-08 17:48:30 +0900

    Second commit

--- find_new.py
+++ find_new.py
@@ -1,45 +1,60 @@
 # import libraries
 import urllib.request
 from feedgen.feed import FeedGenerator
-from post_parser import post_title
+from post_parser import post_title, post_author, post_time, post_files_num
+from misc import is_number
 
 # info
 baseurl = 'http://phya.snu.ac.kr/xe/underbbs/'
 url ='http://phya.snu.ac.kr/xe/index.php?mid=underbbs&category=371' # notices only
 
-# make FeedGenerator
-fg = FeedGenerator()
-fg.id('asdf')
-fg.title('RSS Feed of SNU Physics Board - physics.snu.ac.kr')
-fg.author({'name':'Seungwon Park','email':'yyyyy@snu.ac.kr'})
-fg.link(href='asdf')
-fg.subtitle('SNU Physics RSS')
-fg.language('ko')
 
 f = open('srl_list.txt','r')
 num = f.read().split(',')
 f.close()
 f = open('srl_list.txt','a')
 
-
 response = urllib.request.urlopen(url)
 data = response.read()
 text = data.decode('utf-8')
 
+count_new = 0
+srl_arr = []
+
+
 text_splitted = text.split('document_srl=')
 for i in range(1,len(text_splitted)):
 	srl = text_splitted[i].split('">')[0].split('#comment')[0]
-	if(srl not in num):
-		f.write(',' + srl)
-		print('New post found : ' + srl)
-		fe = fg.add_entry()
-		fe.id(baseurl + srl)
-		fe.title('Test')
-		fe.author({'name':'Seungwon Park','email':'yyyyy@snu.ac.kr'})
-		fe.link(href = baseurl + srl)
-
+	if(is_number(srl)):
+		srl_arr.append(srl)
+		if(srl not in num):
+			count_new += 1
+			f.write(',' + srl)
+			print('New post found : ' + srl)
 
 f.close()
 
-atomfeed = fg.atom_str(pretty=True)
-fg.atom_file('atom.xml')
+if(count_new != 0):
+	print('Started generating feed...')
+	# make FeedGenerator
+	fg = FeedGenerator()
+	fg.id('asdf')
+	fg.title('RSS Feed of SNU Physics Board - physics.snu.ac.kr')
+	fg.author({'name':'Seungwon Park','email':'yyyyy@snu.ac.kr'})
+	fg.link(href='asdf')
+	fg.subtitle('SNU Physics Board RSS')
+	fg.language('ko')
+	for srl in srl_arr:
+		print('Parsing post #' + srl + '...')
+		fe = fg.add_entry()
+		fe.id(baseurl + srl)
+		fe.title(post_title(srl))
+		fe.author({'name':post_author(srl),'email':'yyyyy@snu.ac.kr'})
+		fe.link(href = baseurl + srl)
+
+	atomfeed = fg.atom_str(pretty=True)
+	fg.atom_file('atom.xml')
+	print('Added ' + str(count_new) + ' posts to feed.')
+
+else:
+	print('Posts are up-to-date.')
--- post_parser.py
+++ post_parser.py
@@ -5,19 +5,30 @@ def post_title(srl):
 	post_url = baseurl + srl
 	response = urllib.request.urlopen(post_url)
 	data = response.read()
-	text = data.encode('utf-8')
+	text = data.decode('utf-8')
 	return text.split('<title>')[1].split('</title>')[0]
 
 def post_author(srl):
 	post_url = baseurl + srl
 	response = urllib.request.urlopen(post_url)
 	data = response.read()
-	text = data.encode('utf-8')
+	text = data.decode('utf-8')
 	return text.split('author" onclick="return false">')[1].split('</a>')[0]
 
 def post_time(srl):
 	post_url = baseurl + srl
 	response = urllib.request.urlopen(post_url)
 	data = response.read()
-	text = data.encode('utf-8')
-	return text.split('<p class="time">')[1].split('</p>')[0].replace('\t','')
+	text = data.decode('utf-8')
+	return text.split('<p class="time">')[1].split('</p>')[0].replace('\t','').replace('\n','')
+
+
+def post_files_num(srl): 
+	post_url = baseurl + srl
+	response = urllib.request.urlopen(post_url)
+	data = response.read()
+	text = data.decode('utf-8')
+	try:
+		return text.split('[<strong>')[1].split('</strong>]')[0]
+	except:
+		return 0
