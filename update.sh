dd60d6e1579b77f713ee3809ad7afda93aaee225
Seungwon Park <psw14041@gmail.com>
2017-06-20 17:16:43 +0900

    Revert "Made update.sh to write log.txt"
    This reverts commit 4be33c3c5e959cb982295d3d4ab5b1b13d6f3226.

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 cd /var/www/html/rss/physics
-{ time python3 update_notices.py 2> command.err ; } &>> log.txt
-{ time python3 update_general.py 2> command.err ; } &>> log.txt
+python3 update_notices.py
+python3 update_general.py
