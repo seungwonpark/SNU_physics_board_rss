4be33c3c5e959cb982295d3d4ab5b1b13d6f3226
Seungwon Park <yyyyy@snu.ac.kr>
2017-06-20 12:02:20 +0900

    Made update.sh to write log.txt

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 cd /var/www/html/rss/physics
-python3 update_notices.py
-python3 update_general.py
+{ time python3 update_notices.py 2> command.err ; } &>> log.txt
+{ time python3 update_general.py 2> command.err ; } &>> log.txt
