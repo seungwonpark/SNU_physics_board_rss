05605eab1b0ea6283844dd93c1cff6c715c3679e
Seungwon Park <psw14041@gmail.com>
2018-02-27 00:00:23 +0900

    fix link from 2017 to 2018

--- subjects/course_under_spring_.php
+++ subjects/course_under_spring_.php
@@ -26,10 +26,10 @@
         <img src=img/Ugrad-summer.png border=0>
         </td>
         <td align=center width=225>
-        <a href=2017_sphy1.xml><b>(계절)물리학1</b> (Physics 1)</a>
+        <a href=2018_sphy1.xml><b>(계절)물리학1</b> (Physics 1)</a>
         </td>
         <td align=center width=225>
-        <a href=2017_sphy2.xml><b>(계절)물리학2</b> (Physics 2)</a>
+        <a href=2018_sphy2.xml><b>(계절)물리학2</b> (Physics 2)</a>
         </td>
 		<table cellpadding=2 cellspacing=1 bgcolor="#FFDFDF">
 		<tr height=1 bgcolor=73b94b><td colspan=4></td></tr>
@@ -39,7 +39,7 @@
 		<b>물리학1 (Physics1)</b></p>
 		<?php
 			for($i=1;$i<=12;$i++){
-				echo '<a href=2017_gp1_' . sprintf('%03d',$i) . '.xml><b>' . $i . '반</b></a> ';
+				echo '<a href=2018_gp1_' . sprintf('%03d',$i) . '.xml><b>' . $i . '반</b></a> ';
 				if($i == 6){
 					echo '<br><br>';
 				}
@@ -47,23 +47,23 @@
 		?>
 		<br>
 		<br>
-		<a href=2017_fp001.xml><b>물리의 기본1 (001)</b><br>(Foundation of Physics I)</a></p>
-		<a href=2017_fp002.xml><b>물리의 기본1 (002)</b><br>(Foundation of Physics II)</a></p>
-		<a href=2017_bp.xml><b>기초물리학1 </b><br>(Basic Physics I)</a> </p>
+		<a href=2018_fp001.xml><b>물리의 기본1 (001)</b><br>(Foundation of Physics I)</a></p>
+		<a href=2018_fp002.xml><b>물리의 기본1 (002)</b><br>(Foundation of Physics II)</a></p>
+		<a href=2018_bp.xml><b>기초물리학1 </b><br>(Basic Physics I)</a> </p>
 		</td>
 
                 <td align=center width=225>
-		<a href=2017_hphy.xml><b>고급물리학1</b> <br>(Honor Physics 1)</a></p>
-		<a href=2017_phy001.xml><b>(단)물리학 001</b><br>(Physics 001)</a></p>
-		<a href=2017_phy002.xml><b>(단)물리학 002</b><br>(Physics 002)</p>
-		<a href=2017_phy003.xml><b>(단)물리학 003</b><br>(Physics 003)</p>
+		<a href=2018_hphy.xml><b>고급물리학1</b> <br>(Honor Physics 1)</a></p>
+		<a href=2018_phy001.xml><b>(단)물리학 001</b><br>(Physics 001)</a></p>
+		<a href=2018_phy002.xml><b>(단)물리학 002</b><br>(Physics 002)</p>
+		<a href=2018_phy003.xml><b>(단)물리학 003</b><br>(Physics 003)</p>
 		</td>
 
 		<td align=center width=225>
-		<a href=2017_cpfhp.xml><b>물리학의 개념과 역사</b> <br>(Concepts of Physics From Historical Perspectives)</a></p> 
-		<a href=2017_qthc.xml><b>양자개념과 인류문명</b><br>(Quantum Theory and Human Civilization)</a></p>
+		<a href=2018_cpfhp.xml><b>물리학의 개념과 역사</b> <br>(Concepts of Physics From Historical Perspectives)</a></p> 
+		<a href=2018_qthc.xml><b>양자개념과 인류문명</b><br>(Quantum Theory and Human Civilization)</a></p>
 		<s><b>미시세계와 거시세계</b><br>(Micro-world and Macro-world)</s></p>
-		<a href=2017_energy.xml><b>에너지</b><br>(Energy)</a>
+		<a href=2018_energy.xml><b>에너지</b><br>(Energy)</a>
 		</td></tr>
 
                 <tr height=1 bgcolor=73b94b><td colspan=4></td></tr>
@@ -72,14 +72,14 @@
                 <tr height=100>
                 <td align=left width=130><img src=img/2year.png></td>
                 <td align=center width=220>
-		<a href=2017_mech_s.xml><b>단학기 역학</b> <br>(Mechnics: Short Couse)</a></p>
+		<a href=2018_mech_s.xml><b>단학기 역학</b> <br>(Mechnics: Short Couse)</a></p>
 		</td>
 		<td align=center width=220>
-		<a href=2017_mech1.xml><b>역학1</b><br>(Mechanics 1)</a></p>
-		<!--a href=2017_em1><b>역학1 연습</b><br>(Exercises in Mechanics 1)</a-->
+		<a href=2018_mech1.xml><b>역학1</b><br>(Mechanics 1)</a></p>
+		<!--a href=2018_em1><b>역학1 연습</b><br>(Exercises in Mechanics 1)</a-->
 		</td>
                 <td align=center width=220><a href=></a> 
-		<a href=2017_fmp.xml ><b>현대 물리학의 기초</b> <br>(Foundations of Modern Physics)</a> </p>
+		<a href=2018_fmp.xml ><b>현대 물리학의 기초</b> <br>(Foundations of Modern Physics)</a> </p>
 		</td></tr>
                 <tr height=1 bgcolor=4db37a><td colspan=4></td></tr>
 
@@ -88,17 +88,17 @@
                 <tr height=130>
                 <td align=left width=50><img src=img/3year.png></td>
 		<td align=center width=220>
-		<a href=2017_qp_s.xml><b>(단) 양자물리 1</b> <br>(Quantum Physics 1)</a></p>
-		<a href=2017_qphy1.xml><b>양자물리1</b><br>(Quantum Physics 1)</a></p>
-		<!--a href=2017_eqp1><b>양자물리 1 연습</b> <br>(Exercises in Quantum Physics 1)</a-->
+		<a href=2018_qp_s.xml><b>(단) 양자물리 1</b> <br>(Quantum Physics 1)</a></p>
+		<a href=2018_qphy1.xml><b>양자물리1</b><br>(Quantum Physics 1)</a></p>
+		<!--a href=2018_eqp1><b>양자물리 1 연습</b> <br>(Exercises in Quantum Physics 1)</a-->
 		</td>
                 <td align=center width=220>
-		<a href=2017_emwave.xml><b>전자기파와 광학</b><br>(Electromagnetic Waves and Optics)</a></p>
-		<!--a href=2017_eewo><b>전자기파와 광학 연습</b> <br>(Exercises in Electromagnetic Waves <br>and Optics)</a--> 
+		<a href=2018_emwave.xml><b>전자기파와 광학</b><br>(Electromagnetic Waves and Optics)</a></p>
+		<!--a href=2018_eewo><b>전자기파와 광학 연습</b> <br>(Exercises in Electromagnetic Waves <br>and Optics)</a--> 
 		</td>
                 <td align=center width=220>
-		<a href=2017_mmp.xml><b>물리수학 </b><br>(Mathematical Methods of Physics)</a> </p>
-		<a href=2017_ipl.xml><b>중급물리실험 1</b> <br>(Intermediate Physics Lab.I)</a> 
+		<a href=2018_mmp.xml><b>물리수학 </b><br>(Mathematical Methods of Physics)</a> </p>
+		<a href=2018_ipl.xml><b>중급물리실험 1</b> <br>(Intermediate Physics Lab.I)</a> 
 		</td>
 		</tr>
                 <tr height=1 bgcolor=41b7ab><td colspan=4></td></tr>
@@ -106,16 +106,16 @@
                 <tr height=120>
                 <td align=left width=50><img src=img/4year.png></td>
                 <td align=center width=220>
-		<a href=2017_ibp.xml><b>생물계 물리입문</b> <br>(Introductory biological physics)</a></p>
-		<a href=2017_is1.xml><b>물리연구 1</b><br>(Independent Study 1)</a>
+		<a href=2018_ibp.xml><b>생물계 물리입문</b> <br>(Introductory biological physics)</a></p>
+		<a href=2018_is1.xml><b>물리연구 1</b><br>(Independent Study 1)</a>
 		</td>
-                <td align=center width=220><a href=2017_rst><b>상대론과 시공간</b> <br>(Relativity and Space time)</a></p>
-		<A href=2017_ps.xml><b>고체의 성질</b><br>(Properties of Solids)</a>
+                <td align=center width=220><a href=2018_rst><b>상대론과 시공간</b> <br>(Relativity and Space time)</a></p>
+		<A href=2018_ps.xml><b>고체의 성질</b><br>(Properties of Solids)</a>
 		</td>
                 <td align=center width=220>
-		<!--a href=2017_fm><b>유체역학</b> <br>(Fluid Mechanics)</a></p-->
-		<a href=2017_ao.xml><b>고급광학</b><br>(Advanced Optics)</a></p>
-		<A href=2017_spb.xml><b>고급물리실험</b><br>(Senoir Physics Lab.)</a></td>
+		<!--a href=2018_fm><b>유체역학</b> <br>(Fluid Mechanics)</a></p-->
+		<a href=2018_ao.xml><b>고급광학</b><br>(Advanced Optics)</a></p>
+		<A href=2018_spb.xml><b>고급물리실험</b><br>(Senoir Physics Lab.)</a></td>
 		</tr>
                 <tr height=1 bgcolor=33bace><td colspan=4></td></tr>
 
@@ -137,10 +137,10 @@
         <img src=img/Ugrad-summer.png border=0>
         </td>
         <td align=center width=225>
-        <a href=2017_sphy1_c.xml><b>(계절)물리학1</b> (Physics 1)</a>
+        <a href=2018_sphy1_c.xml><b>(계절)물리학1</b> (Physics 1)</a>
         </td>
         <td align=center width=225>
-        <a href=2017_sphy2_c.xml><b>(계절)물리학2</b> (Physics 2)</a>
+        <a href=2018_sphy2_c.xml><b>(계절)물리학2</b> (Physics 2)</a>
         </td>
 		<table cellpadding=2 cellspacing=1 bgcolor="#DFFFDF">
 		<tr height=1 bgcolor=73b94b><td colspan=4></td></tr>
@@ -150,7 +150,7 @@
 		<b>물리학1 (Physics1)</b></p>
 		<?php
 			for($i=1;$i<=12;$i++){
-				echo '<a href=2017_gp1_' . sprintf('%03d',$i) . '_c.xml><b>' . $i . '반</b></a> ';
+				echo '<a href=2018_gp1_' . sprintf('%03d',$i) . '_c.xml><b>' . $i . '반</b></a> ';
 				if($i == 6){
 					echo '<br><br>';
 				}
@@ -158,23 +158,23 @@
 		?>
 		<br>
 		<br>
-		<a href=2017_fp001_c.xml><b>물리의 기본1 (001)</b><br>(Foundation of Physics I)</a></p>
-		<a href=2017_fp002_c.xml><b>물리의 기본1 (002)</b><br>(Foundation of Physics II)</a></p>
-		<a href=2017_bp_c.xml><b>기초물리학1 </b><br>(Basic Physics I)</a> </p>
+		<a href=2018_fp001_c.xml><b>물리의 기본1 (001)</b><br>(Foundation of Physics I)</a></p>
+		<a href=2018_fp002_c.xml><b>물리의 기본1 (002)</b><br>(Foundation of Physics II)</a></p>
+		<a href=2018_bp_c.xml><b>기초물리학1 </b><br>(Basic Physics I)</a> </p>
 		</td>
 
                 <td align=center width=225>
-		<a href=2017_hphy_c.xml><b>고급물리학1</b> <br>(Honor Physics 1)</a></p>
-		<a href=2017_phy001_c.xml><b>(단)물리학 001</b><br>(Physics 001)</a></p>
-		<a href=2017_phy002_c.xml><b>(단)물리학 002</b><br>(Physics 002)</p>
-		<a href=2017_phy003_c.xml><b>(단)물리학 003</b><br>(Physics 003)</p>
+		<a href=2018_hphy_c.xml><b>고급물리학1</b> <br>(Honor Physics 1)</a></p>
+		<a href=2018_phy001_c.xml><b>(단)물리학 001</b><br>(Physics 001)</a></p>
+		<a href=2018_phy002_c.xml><b>(단)물리학 002</b><br>(Physics 002)</p>
+		<a href=2018_phy003_c.xml><b>(단)물리학 003</b><br>(Physics 003)</p>
 		</td>
 
 		<td align=center width=225>
-		<a href=2017_cpfhp_c.xml><b>물리학의 개념과 역사</b> <br>(Concepts of Physics From Historical Perspectives)</a></p> 
-		<a href=2017_qthc_c.xml><b>양자개념과 인류문명</b><br>(Quantum Theory and Human Civilization)</a></p>
+		<a href=2018_cpfhp_c.xml><b>물리학의 개념과 역사</b> <br>(Concepts of Physics From Historical Perspectives)</a></p> 
+		<a href=2018_qthc_c.xml><b>양자개념과 인류문명</b><br>(Quantum Theory and Human Civilization)</a></p>
 		<s><b>미시세계와 거시세계</b><br>(Micro-world and Macro-world)</s></p>
-		<a href=2017_energy_c.xml><b>에너지</b><br>(Energy)</a>
+		<a href=2018_energy_c.xml><b>에너지</b><br>(Energy)</a>
 		</td></tr>
 
                 <tr height=1 bgcolor=73b94b><td colspan=4></td></tr>
@@ -183,14 +183,14 @@
                 <tr height=100>
                 <td align=left width=130><img src=img/2year.png></td>
                 <td align=center width=220>
-		<a href=2017_mech_s_c.xml><b>단학기 역학</b> <br>(Mechnics: Short Couse)</a></p>
+		<a href=2018_mech_s_c.xml><b>단학기 역학</b> <br>(Mechnics: Short Couse)</a></p>
 		</td>
 		<td align=center width=220>
-		<a href=2017_mech1_c.xml><b>역학1</b><br>(Mechanics 1)</a></p>
-		<!--a href=2017_em1><b>역학1 연습</b><br>(Exercises in Mechanics 1)</a-->
+		<a href=2018_mech1_c.xml><b>역학1</b><br>(Mechanics 1)</a></p>
+		<!--a href=2018_em1><b>역학1 연습</b><br>(Exercises in Mechanics 1)</a-->
 		</td>
                 <td align=center width=220><a href=></a> 
-		<a href=2017_fmp_c.xml ><b>현대 물리학의 기초</b> <br>(Foundations of Modern Physics)</a> </p>
+		<a href=2018_fmp_c.xml ><b>현대 물리학의 기초</b> <br>(Foundations of Modern Physics)</a> </p>
 		</td></tr>
                 <tr height=1 bgcolor=4db37a><td colspan=4></td></tr>
 
@@ -199,17 +199,17 @@
                 <tr height=130>
                 <td align=left width=50><img src=img/3year.png></td>
 		<td align=center width=220>
-		<a href=2017_qp_s_c.xml><b>(단) 양자물리 1</b> <br>(Quantum Physics 1)</a></p>
-		<a href=2017_qphy1_c.xml><b>양자물리1</b><br>(Quantum Physics 1)</a></p>
-		<!--a href=2017_eqp1><b>양자물리 1 연습</b> <br>(Exercises in Quantum Physics 1)</a-->
+		<a href=2018_qp_s_c.xml><b>(단) 양자물리 1</b> <br>(Quantum Physics 1)</a></p>
+		<a href=2018_qphy1_c.xml><b>양자물리1</b><br>(Quantum Physics 1)</a></p>
+		<!--a href=2018_eqp1><b>양자물리 1 연습</b> <br>(Exercises in Quantum Physics 1)</a-->
 		</td>
                 <td align=center width=220>
-		<a href=2017_emwave_c.xml><b>전자기파와 광학</b><br>(Electromagnetic Waves and Optics)</a></p>
-		<!--a href=2017_eewo><b>전자기파와 광학 연습</b> <br>(Exercises in Electromagnetic Waves <br>and Optics)</a--> 
+		<a href=2018_emwave_c.xml><b>전자기파와 광학</b><br>(Electromagnetic Waves and Optics)</a></p>
+		<!--a href=2018_eewo><b>전자기파와 광학 연습</b> <br>(Exercises in Electromagnetic Waves <br>and Optics)</a--> 
 		</td>
                 <td align=center width=220>
-		<a href=2017_mmp_c.xml><b>물리수학 </b><br>(Mathematical Methods of Physics)</a> </p>
-		<a href=2017_ipl_c.xml><b>중급물리실험 1</b> <br>(Intermediate Physics Lab.I)</a> 
+		<a href=2018_mmp_c.xml><b>물리수학 </b><br>(Mathematical Methods of Physics)</a> </p>
+		<a href=2018_ipl_c.xml><b>중급물리실험 1</b> <br>(Intermediate Physics Lab.I)</a> 
 		</td>
 		</tr>
                 <tr height=1 bgcolor=41b7ab><td colspan=4></td></tr>
@@ -217,16 +217,16 @@
                 <tr height=120>
                 <td align=left width=50><img src=img/4year.png></td>
                 <td align=center width=220>
-		<a href=2017_ibp_c.xml><b>생물계 물리입문</b> <br>(Introductory biological physics)</a></p>
-		<a href=2017_is1_c.xml><b>물리연구 1</b><br>(Independent Study 1)</a>
+		<a href=2018_ibp_c.xml><b>생물계 물리입문</b> <br>(Introductory biological physics)</a></p>
+		<a href=2018_is1_c.xml><b>물리연구 1</b><br>(Independent Study 1)</a>
 		</td>
-                <td align=center width=220><a href=2017_rst><b>상대론과 시공간</b> <br>(Relativity and Space time)</a></p>
-		<A href=2017_ps_c.xml><b>고체의 성질</b><br>(Properties of Solids)</a>
+                <td align=center width=220><a href=2018_rst><b>상대론과 시공간</b> <br>(Relativity and Space time)</a></p>
+		<A href=2018_ps_c.xml><b>고체의 성질</b><br>(Properties of Solids)</a>
 		</td>
                 <td align=center width=220>
-		<!--a href=2017_fm><b>유체역학</b> <br>(Fluid Mechanics)</a></p-->
-		<a href=2017_ao_c.xml><b>고급광학</b><br>(Advanced Optics)</a></p>
-		<A href=2017_spb_c.xml><b>고급물리실험</b><br>(Senoir Physics Lab.)</a></td>
+		<!--a href=2018_fm><b>유체역학</b> <br>(Fluid Mechanics)</a></p-->
+		<a href=2018_ao_c.xml><b>고급광학</b><br>(Advanced Optics)</a></p>
+		<A href=2018_spb_c.xml><b>고급물리실험</b><br>(Senoir Physics Lab.)</a></td>
 		</tr>
                 <tr height=1 bgcolor=33bace><td colspan=4></td></tr>
 
@@ -248,10 +248,10 @@
         <img src=img/Ugrad-summer.png border=0>
         </td>
         <td align=center width=225>
-        <a href=2017_sphy1_h.xml><b>(계절)물리학1</b> (Physics 1)</a>
+        <a href=2018_sphy1_h.xml><b>(계절)물리학1</b> (Physics 1)</a>
         </td>
         <td align=center width=225>
-        <a href=2017_sphy2_h.xml><b>(계절)물리학2</b> (Physics 2)</a>
+        <a href=2018_sphy2_h.xml><b>(계절)물리학2</b> (Physics 2)</a>
         </td>
 		<table cellpadding=2 cellspacing=1 bgcolor="#DFDFFF">
 		<tr height=1 bgcolor=73b94b><td colspan=4></td></tr>
@@ -261,7 +261,7 @@
 		<b>물리학1 (Physics1)</b></p>
 		<?php
 			for($i=1;$i<=12;$i++){
-				echo '<a href=2017_gp1_' . sprintf('%03d',$i) . '_h.xml><b>' . $i . '반</b></a> ';
+				echo '<a href=2018_gp1_' . sprintf('%03d',$i) . '_h.xml><b>' . $i . '반</b></a> ';
 				if($i == 6){
 					echo '<br><br>';
 				}
@@ -269,23 +269,23 @@
 		?>
 		<br>
 		<br>
-		<a href=2017_fp001_h.xml><b>물리의 기본1 (001)</b><br>(Foundation of Physics I)</a></p>
-		<a href=2017_fp002_h.xml><b>물리의 기본1 (002)</b><br>(Foundation of Physics II)</a></p>
-		<a href=2017_bp_h.xml><b>기초물리학1 </b><br>(Basic Physics I)</a> </p>
+		<a href=2018_fp001_h.xml><b>물리의 기본1 (001)</b><br>(Foundation of Physics I)</a></p>
+		<a href=2018_fp002_h.xml><b>물리의 기본1 (002)</b><br>(Foundation of Physics II)</a></p>
+		<a href=2018_bp_h.xml><b>기초물리학1 </b><br>(Basic Physics I)</a> </p>
 		</td>
 
                 <td align=center width=225>
-		<a href=2017_hphy_h.xml><b>고급물리학1</b> <br>(Honor Physics 1)</a></p>
-		<a href=2017_phy001_h.xml><b>(단)물리학 001</b><br>(Physics 001)</a></p>
-		<a href=2017_phy002_h.xml><b>(단)물리학 002</b><br>(Physics 002)</p>
-		<a href=2017_phy003_h.xml><b>(단)물리학 003</b><br>(Physics 003)</p>
+		<a href=2018_hphy_h.xml><b>고급물리학1</b> <br>(Honor Physics 1)</a></p>
+		<a href=2018_phy001_h.xml><b>(단)물리학 001</b><br>(Physics 001)</a></p>
+		<a href=2018_phy002_h.xml><b>(단)물리학 002</b><br>(Physics 002)</p>
+		<a href=2018_phy003_h.xml><b>(단)물리학 003</b><br>(Physics 003)</p>
 		</td>
 
 		<td align=center width=225>
-		<a href=2017_cpfhp_h.xml><b>물리학의 개념과 역사</b> <br>(Concepts of Physics From Historical Perspectives)</a></p> 
-		<a href=2017_qthc_h.xml><b>양자개념과 인류문명</b><br>(Quantum Theory and Human Civilization)</a></p>
+		<a href=2018_cpfhp_h.xml><b>물리학의 개념과 역사</b> <br>(Concepts of Physics From Historical Perspectives)</a></p> 
+		<a href=2018_qthc_h.xml><b>양자개념과 인류문명</b><br>(Quantum Theory and Human Civilization)</a></p>
 		<s><b>미시세계와 거시세계</b><br>(Micro-world and Macro-world)</s></p>
-		<a href=2017_energy_h.xml><b>에너지</b><br>(Energy)</a>
+		<a href=2018_energy_h.xml><b>에너지</b><br>(Energy)</a>
 		</td></tr>
 
                 <tr height=1 bgcolor=73b94b><td colspan=4></td></tr>
@@ -294,14 +294,14 @@
                 <tr height=100>
                 <td align=left width=130><img src=img/2year.png></td>
                 <td align=center width=220>
-		<a href=2017_mech_s_h.xml><b>단학기 역학</b> <br>(Mechnics: Short Couse)</a></p>
+		<a href=2018_mech_s_h.xml><b>단학기 역학</b> <br>(Mechnics: Short Couse)</a></p>
 		</td>
 		<td align=center width=220>
-		<a href=2017_mech1_h.xml><b>역학1</b><br>(Mechanics 1)</a></p>
-		<!--a href=2017_em1><b>역학1 연습</b><br>(Exercises in Mechanics 1)</a-->
+		<a href=2018_mech1_h.xml><b>역학1</b><br>(Mechanics 1)</a></p>
+		<!--a href=2018_em1><b>역학1 연습</b><br>(Exercises in Mechanics 1)</a-->
 		</td>
                 <td align=center width=220><a href=></a> 
-		<a href=2017_fmp_h.xml ><b>현대 물리학의 기초</b> <br>(Foundations of Modern Physics)</a> </p>
+		<a href=2018_fmp_h.xml ><b>현대 물리학의 기초</b> <br>(Foundations of Modern Physics)</a> </p>
 		</td></tr>
                 <tr height=1 bgcolor=4db37a><td colspan=4></td></tr>
 
@@ -310,17 +310,17 @@
                 <tr height=130>
                 <td align=left width=50><img src=img/3year.png></td>
 		<td align=center width=220>
-		<a href=2017_qp_s_h.xml><b>(단) 양자물리 1</b> <br>(Quantum Physics 1)</a></p>
-		<a href=2017_qphy1_h.xml><b>양자물리1</b><br>(Quantum Physics 1)</a></p>
-		<!--a href=2017_eqp1><b>양자물리 1 연습</b> <br>(Exercises in Quantum Physics 1)</a-->
+		<a href=2018_qp_s_h.xml><b>(단) 양자물리 1</b> <br>(Quantum Physics 1)</a></p>
+		<a href=2018_qphy1_h.xml><b>양자물리1</b><br>(Quantum Physics 1)</a></p>
+		<!--a href=2018_eqp1><b>양자물리 1 연습</b> <br>(Exercises in Quantum Physics 1)</a-->
 		</td>
                 <td align=center width=220>
-		<a href=2017_emwave_h.xml><b>전자기파와 광학</b><br>(Electromagnetic Waves and Optics)</a></p>
-		<!--a href=2017_eewo><b>전자기파와 광학 연습</b> <br>(Exercises in Electromagnetic Waves <br>and Optics)</a--> 
+		<a href=2018_emwave_h.xml><b>전자기파와 광학</b><br>(Electromagnetic Waves and Optics)</a></p>
+		<!--a href=2018_eewo><b>전자기파와 광학 연습</b> <br>(Exercises in Electromagnetic Waves <br>and Optics)</a--> 
 		</td>
                 <td align=center width=220>
-		<a href=2017_mmp_h.xml><b>물리수학 </b><br>(Mathematical Methods of Physics)</a> </p>
-		<a href=2017_ipl_h.xml><b>중급물리실험 1</b> <br>(Intermediate Physics Lab.I)</a> 
+		<a href=2018_mmp_h.xml><b>물리수학 </b><br>(Mathematical Methods of Physics)</a> </p>
+		<a href=2018_ipl_h.xml><b>중급물리실험 1</b> <br>(Intermediate Physics Lab.I)</a> 
 		</td>
 		</tr>
                 <tr height=1 bgcolor=41b7ab><td colspan=4></td></tr>
@@ -328,16 +328,16 @@
                 <tr height=120>
                 <td align=left width=50><img src=img/4year.png></td>
                 <td align=center width=220>
-		<a href=2017_ibp_h.xml><b>생물계 물리입문</b> <br>(Introductory biological physics)</a></p>
-		<a href=2017_is1_h.xml><b>물리연구 1</b><br>(Independent Study 1)</a>
+		<a href=2018_ibp_h.xml><b>생물계 물리입문</b> <br>(Introductory biological physics)</a></p>
+		<a href=2018_is1_h.xml><b>물리연구 1</b><br>(Independent Study 1)</a>
 		</td>
-                <td align=center width=220><a href=2017_rst><b>상대론과 시공간</b> <br>(Relativity and Space time)</a></p>
-		<A href=2017_ps_h.xml><b>고체의 성질</b><br>(Properties of Solids)</a>
+                <td align=center width=220><a href=2018_rst><b>상대론과 시공간</b> <br>(Relativity and Space time)</a></p>
+		<A href=2018_ps_h.xml><b>고체의 성질</b><br>(Properties of Solids)</a>
 		</td>
                 <td align=center width=220>
-		<!--a href=2017_fm><b>유체역학</b> <br>(Fluid Mechanics)</a></p-->
-		<a href=2017_ao_h.xml><b>고급광학</b><br>(Advanced Optics)</a></p>
-		<A href=2017_spb_h.xml><b>고급물리실험</b><br>(Senoir Physics Lab.)</a></td>
+		<!--a href=2018_fm><b>유체역학</b> <br>(Fluid Mechanics)</a></p-->
+		<a href=2018_ao_h.xml><b>고급광학</b><br>(Advanced Optics)</a></p>
+		<A href=2018_spb_h.xml><b>고급물리실험</b><br>(Senoir Physics Lab.)</a></td>
 		</tr>
                 <tr height=1 bgcolor=33bace><td colspan=4></td></tr>
 
