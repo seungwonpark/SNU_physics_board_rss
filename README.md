4060d1191b62145c2cbaf8d60905371816d0a08e
seungwon park <yyyyy@snu.ac.kr>
2018-09-02 12:37:41 +0900

    2018 fall, service termination

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 서울대 물리천문학부 [게시판](http://phya.snu.ac.kr/xe/underbbs) rss / SNU phya [board](http://phya.snu.ac.kr/xe/underbbs) rss
 
+## Service will be terminated after 2018 fall. [Read more](https://github.com/seungwonpark/SNU_physics_board_rss/wiki/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%A2%85%EB%A3%8C-%EC%95%88%EB%82%B4---Announcement-about-service-termination)
+
 ![status](https://circleci.com/gh/seungwonpark/SNU_physics_board_rss.svg?style=shield)
 
 Source code of [SNU Physics Board RSS](http://swpark.ddns.net/rss/physics).
--- index.php
+++ index.php
@@ -11,19 +11,29 @@
 
 <h2> 서울대 물리학과 게시판 RSS(베타) </h2>
 <h2> SNU Physics Board RSS(Beta) </h2>
-<h2> <s>텔레그램 봇 <a href="http://t.me/ShaynmanBot">샤인만</a>을 제작한 관계로, 곧 서비스가 종료됩니다.</s> </h2>
-<h2> <s>Since a new Telegram bot, <a href="http://t.me/ShaynmanBot">Shaynman</a>, has been developed, this service will be teminated shortly.</s> </h2>
+<h3> 2018년 2학기 학부 및 대학원 강의 업데이트가 시작되었습니다. (2018.09.02) </h3>
+<h3> We started updating undergrad/grad courses for 2018 fall semester. (2018.09.02) </h3>
+<h3> 본 서비스는 2018년 2학기를 마지막으로 종료될 예정입니다. <a href="https://github.com/seungwonpark/SNU_physics_board_rss/wiki/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%A2%85%EB%A3%8C-%EC%95%88%EB%82%B4---Announcement-about-service-termination">자세히</a> </h3>
+<h3> This service will be terminated after 2018 fall semester. <a href="https://github.com/seungwonpark/SNU_physics_board_rss/wiki/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%A2%85%EB%A3%8C-%EC%95%88%EB%82%B4---Announcement-about-service-termination">Read more</a> </h3>
 
 <img src="https://circleci.com/gh/seungwonpark/SNU_physics_board_rss.svg?style=shield" alt="CircleCI build status">
 
 <h3>
-학사 게시판(Announcement) : <a href="notices.xml" target="_blank">공지(Notices)</a> / 
+학사 게시판(Announcement): <a href="notices.xml" target="_blank">공지(Notices)</a> / 
 <a href="general.xml" target="_blank">일반(General)</a> -- 
 <small> <a href="http://phya.snu.ac.kr/xe/underbbs" target="_blank">원문(Source)</a> </small>
 <br>
 
-강의 게시판(Course Board) : <a href="subjects/course_under_spring_.php">학부(Undergrad.)</a> / 
-<a href="subjects/course_grad_spring_.php">석박사과정(Grad.)</a> -- 
+강의 게시판(Course Board): <!--<a href="subjects/course_under_spring_.php">학부(Undergrad.)</a> / 
+<a href="subjects/course_grad_spring_.php">석박사과정(Grad.)</a> -- -->
+
+<br>
+링크: 강의게시판 링크가 http://physics.snu.ac.kr/php/subject_list/list_db.php?id=2018_em 와 같을 때,
+<ul>
+	<li>강의게시판(Announcements): http://swpark.ddns.net/rss/physics/subjects/2018_em.xml</li>
+	<li>강의자료 게시판(Course Materials): http://swpark.ddns.net/rss/physics/subjects/2018_em_c.xml</li>
+	<li>과제 게시판(Homework): http://swpark.ddns.net/rss/physics/subjects/2018_em_h.xml</li>
+</ul>
 <small> <a href="http://phya.snu.ac.kr/page3/index.php" target="_blank">원문(Source)</a> </small>
 <br>
 </h3>
@@ -32,14 +42,14 @@
 *Announcement RSS is bit slow. Work in progress. <br>
 <br>
 
-*경고 : 이 서비스는 언제든지 중단될 수 있습니다. <br>
-*Warning : This service can be discontinued any time. <br>
+*경고: 이 서비스는 언제든지 중단될 수 있습니다. <br>
+*Warning: This service can be discontinued any time. <br>
 
 <br>
 
 <small>
-만든이 : 박승원(서울대학교 물리천문학부 물리학전공 17학번) / yyyyy at snu dot ac dot kr <br>
-Credit : Seungwon Park(SNU Dept. of Physics/Astronomy Physics major, '17) / yyyyy at snu dot ac dot kr <br>
+만든이: 박승원(서울대학교 물리천문학부 물리학전공 17학번) / yyyyy at snu dot ac dot kr <br>
+Credit: Seungwon Park(SNU Dept. of Physics/Astronomy Physics major, '17) / yyyyy at snu dot ac dot kr <br>
 </small>
 <br>
 <a href="https://github.com/seungwonpark/SNU_physics_board_rss">This project on GitHub</a>
